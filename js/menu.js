6e53025dd77374999ba8c16ec9161a42c0016fe2
Henry Saver <hesa1262@colorado.edu>
2021-06-12 00:29:25 -0600

    Updating Navbar for mobile

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-    if (document.documentElement.clientWidth > 480) {
-      $("#myNav").addClass("navbar-fixed-top");
+// $(document).ready(function() {
+//     if (document.documentElement.clientWidth > 480) {
+//       $("#myNav").addClass("navbar-fixed-top");
+//     } else {
+//       $("#myNav").removeClass("navbar-fixed-top");
+//     }
+//   });
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
     } else {
-      $("#myNav").removeClass("navbar-fixed-top");
+      x.style.display = "block";
     }
-  });
+  }
